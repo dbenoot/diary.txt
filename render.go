9c5c04ad6ed81a806aa0a7d4c521738f441a4291
dbenoot <dbenoot@gmail.com>
2017-01-03 15:34:00 +0000

    header cleanup

--- render.go
+++ render.go
@@ -12,13 +12,6 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-// - read all files
-//   filepath.walk??
-// - sort files
-//   sort.Strings(strs)
-// - concatenate all the resulting strings
-// - md -> html
-
 package main
 
 import (
