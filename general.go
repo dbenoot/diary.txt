41cd3d1c80cd4f8ccfcf76fb8094860db522b79d
dbenoot <dbenoot@gmail.com>
2017-01-04 15:27:01 +0000

    updated the filterFile function so the files should have the correct format, or will be disregarded from further processing

--- general.go
+++ general.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -15,18 +17,43 @@ func getFileList(wd string) ([]string, error) {
 		return nil
 	})
 
-	fileList = filterFile(fileList)
+	fileList = filterFile(fileList, wd)
 
 	return fileList, err
 }
 
-func filterFile(f []string) []string {
+func filterFile(f []string, wd string) []string {
 
 	var fo []string
 
+	// use regexp [0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{4}_['\\w,\\s-\\.]*\\.md
+	//
+	// meaning
+	// [0-9]{4} : 4 digits
+	// - : the character -
+	// [0-9]{2} : 2 digits
+	// - : the character -
+	// [0-9]{2} : 2 digits
+	// T : the character T
+	// [0-9]{4} : 4 digits
+	// _ : the character _
+	// ['\\w,\\s-\\.]* : text consisting of  ', all word symbols, all whitespace symbols, dashes, dots
+	// \\.md : .md
+	//
+	// Remark the double escape for w, s and . -> otherwise the string parser complains (and '' didn't work...)
+
+	var r = regexp.MustCompile("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{4}_['\\w,\\s-\\.]*\\.md")
+	logdir := filepath.Join(wd, "logs")
+	renderdir := filepath.Join(wd, "rendered")
+
 	for _, file := range f {
-		if strings.Contains(file, "rendered_diary") == false && strings.Contains(file, ".md") {
+		if r.MatchString(file) && strings.Contains(file, renderdir) == false { //&& strings.Contains(file, ".md") {
 			fo = append(fo, file)
+		} else {
+			fi, _ := os.Stat(file)
+			if fi.Mode().IsRegular() == true && strings.Contains(file, renderdir) == false && strings.Contains(file, logdir) == false {
+				fmt.Printf("File was not included in the filterlist %s. Please check filterFile function. \n", file)
+			}
 		}
 	}
 
