45bf3144252b9b1bd46301bd0a446e3f40dfbf26
dbenoot <dbenoot@gmail.com>
2018-04-22 22:45:23 +0200

    adding attachments - first step

--- main.go
+++ main.go
@@ -45,13 +45,13 @@ func main() {
 
 	// check that the local config directory (sd) exists and if not create a preliminary config file
 
-	diary.wd, diary.copyPins = setWorkDir()
+	diary.wd = setWorkDir()
 
 	// Create and load diary specific config file (only the pins at this moment)
 	// This makes it possible to sync diary specific settings when using cloud sync, as this sync file is in the diary folder.
 	// Splitting up the config files gives the ability to run the executable from anywhere and not only in the diary folder and sanely keep diary specific settings when syncing.
 
-	diary.pins, localCfgFile = setPins(diary.wd)
+	diary.pins, localCfgFile, diary.copyPins = setPins(diary.wd)
 
 	// make sure the necessary paths are present
 
@@ -91,46 +91,15 @@ func main() {
 	indexAllPinFlag := pinCommand.Bool("indexall", false, "Shows all prespecifed pins with their distinct answers.")
 	listPinFlag := pinCommand.String("list", "", "Shows all items for a specific pin.")
 
+	attachCommand := flag.NewFlagSet("attach", flag.ExitOnError)
+	attachImageFlag := attachCommand.String("image", "", "Insert an image.")
+	attachFileFlag := attachCommand.String("file", "", "Attach a file.")
+	attachEntryFlag := attachCommand.String("entry", "", "Entry to which the attachment should be attached.")
 	// What to show when no arguments are defined
 
 	if len(os.Args) == 1 {
-		fmt.Println("Please provide secondary command.")
-		fmt.Println("")
-		fmt.Println("The following commands can be issued:")
-		fmt.Println("")
-		fmt.Println("create          Creates a new journal entry")
-		fmt.Println("  -title        Title your diary entry. Default is today's date.")
-		fmt.Println("  -date         Specify the date for your diary entry. Format should be yyyymmddThhmm (e.g. 20060102T1504). Default is today.")
-		fmt.Println("  -text         Add text to the diary entry. Especially useful for short notes, for larger notes an editor is best used. Default is empty.")
-		fmt.Println("  -tag          Add tags (comma-separated) to journal entry. Can also be added using editor. Default is empty.")
-		fmt.Println("  -pin          Specify if the pins should be present. Notation example: -pin=false (include equal sign). Default is true.")
-		fmt.Println("  -copypin      Copies the pin content from the last written journal entry.")
-		fmt.Println("")
-		fmt.Println("render          Renders your diary entries to a single markdown and html document located in the rendered folder in your diary home directory.")
-		fmt.Println("  -tag          Render journal entries with a specific tag. Default is empty.")
-		fmt.Println("  -year         Render journal entries from a specific year. Default is empty.")
-		fmt.Println("  -month        Render journal entries from a specific month. Default is empty. Format is 2 digit numeric.")
-		fmt.Println("")
-		fmt.Println("search          Search your journal entries")
-		fmt.Println("  -tag          Search for entries with a specific tag. Default is empty.")
-		fmt.Println("  -text         Search text. Default is empty.")
-		fmt.Println("  -year         Search journal items for a specific year. Default is empty.")
-		fmt.Println("  -month        Search journal items for a specific month. Default is empty.")
-		fmt.Println("  -v            Set the output verbosity. Default is false.")
-		fmt.Println("")
-		fmt.Println("tag")
-		fmt.Println("  -index        Shows all tags.")
-		fmt.Println("  -list         Shows all journal entries for a specific tag.")
-		fmt.Println("  -v            Set the output verbosity. Default is false.")
-		fmt.Println("")
-		fmt.Println("pin             Administrate the journal pins.")
-		fmt.Println("  -add          Add a pin. Default is empty.")
-		fmt.Println("  -remove       Remove a pin. Default is empty.")
-		fmt.Println("  -index        Shows all prespecified pins.")
-		fmt.Println("  -indexall     Shows all prespecifed pins with their distinct answers.")
-		fmt.Println("  -list         Shows all items for a specific pin.")
-		fmt.Println("")
-		fmt.Println("stat            Some basic statistics about your diary. Output is saved in the logs folder in your diary home directory.")
+
+		fmt.Println("Please provide arguments. Type 'diarytxt help' for more information.")
 
 	} else {
 
@@ -151,8 +120,13 @@ func main() {
 			statistics(diary.wd, tStr)
 		case "shame":
 			shame(diary.wd)
+		case "help":
+			printHelp()
+		case "attach":
+			attachCommand.Parse(os.Args[2:])
 		default:
 			fmt.Printf("%q is not valid command.\n", os.Args[1])
+			fmt.Println("Use the help command 'diarytxt help' for help.")
 			os.Exit(2)
 		}
 	}
@@ -178,9 +152,13 @@ func main() {
 	if pinCommand.Parsed() {
 		pin(*addPinFlag, *removePinFlag, *listPinFlag, *indexPinFlag, *indexAllPinFlag, diary.wd, diary.pins, localCfgFile, os.Args)
 	}
+
+	if attachCommand.Parsed() {
+		attach(*attachImageFlag, *attachFileFlag, *attachEntryFlag)
+	}
 }
 
-func setWorkDir() (string, bool) {
+func setWorkDir() string {
 	usr, err := user.Current()
 	check(err)
 
@@ -192,7 +170,6 @@ func setWorkDir() (string, bool) {
 		os.Create(cfgFile)
 		var cfg, _ = ini.LooseLoad(cfgFile)
 		_, _ = cfg.Section("general").NewKey("home", "")
-		_, _ = cfg.Section("general").NewKey("copy_pin_content", "true")
 		err = cfg.SaveTo(cfgFile)
 	}
 
@@ -203,27 +180,32 @@ func setWorkDir() (string, bool) {
 		os.Exit(2)
 	}
 
-	copyPins, err := cfg.Section("general").Key("copy_pin_content").Bool()
-	check(err)
 
-	return cfg.Section("general").Key("home").String(), copyPins
+	// check(err)
+
+	return cfg.Section("general").Key("home").String()
 }
 
-func setPins(wd string) ([]string, string) {
+func setPins(wd string) ([]string, string, bool) {
 	settingsDir := filepath.Join(wd, "settings")
 	localCfgFile := filepath.Join(settingsDir, "local_config.ini")
+
+
 
 	if _, err := os.Stat(localCfgFile); os.IsNotExist(err) {
 		_ = os.MkdirAll(settingsDir, 0755)
 		os.Create(localCfgFile)
 		var localCfg, _ = ini.LooseLoad(localCfgFile)
 		_, _ = localCfg.Section("general").NewKey("pins", "")
+		_, _ = localCfg.Section("general").NewKey("copy_pin_content", "true")
 		err = localCfg.SaveTo(localCfgFile)
 	}
 
 	var localCfg, _ = ini.LooseLoad(localCfgFile)
 
-	return localCfg.Section("general").Key("pins").Strings(","), localCfgFile
+	copyPins, _ := localCfg.Section("general").Key("copy_pin_content").Bool()
+
+	return localCfg.Section("general").Key("pins").Strings(","), localCfgFile, copyPins
 }
 
 func createDirs(wd string) {
@@ -241,4 +223,49 @@ func createDirs(wd string) {
 	if _, err := os.Stat(filedir); os.IsNotExist(err) {
 		_ = os.MkdirAll(filedir, 0755)
 	}
+}
+
+func printHelp() {
+	fmt.Println("Please provide secondary command.")
+	fmt.Println("")
+	fmt.Println("The following commands can be issued:")
+	fmt.Println("")
+	fmt.Println("create          Creates a new journal entry")
+	fmt.Println("  -title        Title your diary entry. Default is today's date.")
+	fmt.Println("  -date         Specify the date for your diary entry. Format should be yyyymmddThhmm (e.g. 20060102T1504). Default is today.")
+	fmt.Println("  -text         Add text to the diary entry. Especially useful for short notes, for larger notes an editor is best used. Default is empty.")
+	fmt.Println("  -tag          Add tags (comma-separated) to journal entry. Can also be added using editor. Default is empty.")
+	fmt.Println("  -pin          Specify if the pins should be present. Notation example: -pin=false (include equal sign). Default is true.")
+	fmt.Println("  -copypin      Copies the pin content from the last written journal entry.")
+	fmt.Println("")
+	fmt.Println("render          Renders your diary entries to a single markdown and html document located in the rendered folder in your diary home directory.")
+	fmt.Println("  -tag          Render journal entries with a specific tag. Default is empty.")
+	fmt.Println("  -year         Render journal entries from a specific year. Default is empty.")
+	fmt.Println("  -month        Render journal entries from a specific month. Default is empty. Format is 2 digit numeric.")
+	fmt.Println("")
+	fmt.Println("search          Search your journal entries")
+	fmt.Println("  -tag          Search for entries with a specific tag. Default is empty.")
+	fmt.Println("  -text         Search text. Default is empty.")
+	fmt.Println("  -year         Search journal items for a specific year. Default is empty.")
+	fmt.Println("  -month        Search journal items for a specific month. Default is empty.")
+	fmt.Println("  -v            Set the output verbosity. Default is false.")
+	fmt.Println("")
+	fmt.Println("tag")
+	fmt.Println("  -index        Shows all tags.")
+	fmt.Println("  -list         Shows all journal entries for a specific tag.")
+	fmt.Println("  -v            Set the output verbosity. Default is false.")
+	fmt.Println("")
+	fmt.Println("pin             Administrate the journal pins.")
+	fmt.Println("  -add          Add a pin. Default is empty.")
+	fmt.Println("  -remove       Remove a pin. Default is empty.")
+	fmt.Println("  -index        Shows all prespecified pins.")
+	fmt.Println("  -indexall     Shows all prespecifed pins with their distinct answers.")
+	fmt.Println("  -list         Shows all items for a specific pin.")
+	fmt.Println("")
+	fmt.Println("stat            Some basic statistics about your diary. Output is saved in the logs folder in your diary home directory.")
+	fmt.Println("")
+	fmt.Println("attach          Attach images and files to a diary entry. File is linked in the diary entry and the file is saved locally in the diary folder.")
+	fmt.Println("  -image        Insert an image in the markdown file. Will be rendered as an image.")
+	fmt.Println("  -file         Attach a file to the diary entry. Will be rendered as a link.")
+
 }
